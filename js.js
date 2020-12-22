d24c19079390ab194a02d83660f44bd0cb8b579e
YoungFanPD <danielsereno432@gmail.com>
2020-12-22 14:16:38 -0600

    first commit

--- js.js
+++ js.js
@@ -39,15 +39,18 @@ function printCars() {
             tableActive = "table-danger";
         }
         html += `<tr class="${tableActive}">
-        <td><input id="brand-box" class="form-control input-sm" type="text"  readonly value="${car.brand}"></td>
-        <td><input id="model-box" class="form-control input-sm" type="text"  readonly value="${car.model}"></td>
-        <td><input class="form-control input-sm" type="text"  readonly value="${car.color}"></td>
-        <td><input class="form-control input-sm" type="text"  readonly value="${car.year}"></td>
-        <td><input class="form-control input-sm" type="text"  readonly value="${car.price}"></td>
+        <td><input id="brand-box-${car.model}" class="form-control input-sm" type="text"  readonly value="${car.brand}"></td>
+        <td><input id="model-box-${car.model}" class="form-control input-sm" type="text"  readonly value="${car.model}"></td>
+        <td><input id="color-box-${car.model}" class="form-control input-sm" type="text"  readonly value="${car.color}"></td>
+        <td><input id="year-box-${car.model}" class="form-control input-sm" type="text"  readonly value="${car.year}"></td>
+        <td><input id="price-box-${car.model}" class="form-control input-sm" type="text"  readonly value="${car.price}"></td>
         <td>
-        <button onclick="deleteCar('${car.model}')" class="btn btn-danger">
-        Update
-    </button>
+                        <button onclick="editRow(getRow('${car.model}'))" id = "btn-update-${car.model}" class="btn btn-primary">
+                            Update
+                        </button>
+                        <button onclick="updateCar('${car.model}')" id = "btn-save-${car.model}" class="btn btn-primary hidden">
+                            Save
+                        </button>
                         <button onclick="deleteCar('${car.model}')" class="btn btn-danger">
                             Eliminar
                         </button>
@@ -63,17 +66,55 @@ function deleteCar(model) {
     printCars();
 }
 
+function getRow(model) {
+    let row = [];
+    row[0] = document.getElementById("brand-box-" + model);
+    row[1] = document.getElementById("model-box-" + model);
+    row[2] = document.getElementById("color-box-" + model);
+    row[3] = document.getElementById("year-box-" + model);
+    row[4] = document.getElementById("price-box-" + model);
+    document.getElementById("btn-update-" + model).style.display = "none";
+    document.getElementById("btn-save-" + model).style.display = "block";
+    return row;
+}
+
+function editRow(row) {
+    row.forEach((box) => box.removeAttribute("readonly"));
+}
+/**function editRow(model) {
+    let brand = document.getElementById("brand-box-" + model);
+    let modelo = document.getElementById("model-box-" + model);
+    let color = document.getElementById("color-box-" + model);
+    let year = document.getElementById("year-box-" + model);
+    let price = document.getElementById("price-box-" + model);
+    document.getElementById("btn-update-" + model).style.display = "none";
+    document.getElementById("btn-save-" + model).style.display = "block";
+    brand.removeAttribute("readonly");
+    modelo.removeAttribute("readonly");
+    color.removeAttribute("readonly");
+    year.removeAttribute("readonly");
+    price.removeAttribute("readonly");
+
+} */
 function updateCar(model) {
-    let car = cars.find((car) => car.model == model);
-    console.log(car.model);
+    const row = getRow(model);
+    cars.forEach((car) => {
+        if (car.model == model) {
+            car.brand = row[0].value;
+            car.model = row[1].value;
+            car.color = row[2].value;
+            car.year = row[3].value;
+            car.price = row[4].value;
+        };
+    });
     printCars();
 }
 
 function searchCar() {
     document.getElementById("search").onkeyup = function() {
-        var res = this.value.toLowerCase();
+        let res = this.value.toLowerCase();
         document.querySelectorAll('table tbody tr').forEach(function(e) {
-            var coincidence = false;
+            let coincidence = false;
             e.querySelectorAll('td').forEach(function(e) {
                 if (e.innerHTML.toLowerCase().indexOf(res) >= 0) {
                     coincidence = true;
